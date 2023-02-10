afd7c552066d53eb99d21bc6d0be51ee4a24a413
Arturo Guzman <gab_r2ro@hotmail.com>
2023-02-09 21:25:18 -0600

    update: update projects section and icon web

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,7 @@ import "../styles/Projects.css";
 import {useTheme} from './Themes';
 import {Container, Row, Col, Collapse, Button} from 'react-bootstrap';
 import geekmitive from '../assets/img/geekmitive.jpg';
+import reactSpringboot from '../assets/img/react-springboot.png';
 import disponible from '../assets/img/disponible.jpg';
 
 const Projects = () =>{
@@ -26,24 +27,24 @@ const Projects = () =>{
                                 </div>
                             </Collapse>
                             <div className="buttons-project">
-                            <Button className={`button-project-${theme}`} href="#" target={"_blank"} rel="noreferrer">Demo</Button>
-                            <Button className={`button-project-${theme}`} href="#" target={"_blank"} rel="noreferrer">Repositorio</Button>
+                            <Button className={`button-project-${theme}`} href="https://geekmitive.netlify.app/" target={"_blank"} rel="noreferrer">Demo</Button>
+                            <Button className={`button-project-${theme}`} href="https://github.com/ArturoCarmona/primitive" target={"_blank"} rel="noreferrer">Repositorio</Button>
                             </div>
                             
                         </div>
                     </Col>
                     <Col sm={6} lg={4} className="col-options">
                         <div className={`card-${theme}`}> 
-                            <img src={disponible} className="card-img" alt="img"></img>       
-                            <h4 className="card-h4" onClick={ () => setOpen2(!open2)}>Proyecto Disponible</h4>
+                            <img src={reactSpringboot} className="card-img" alt="img"></img>       
+                            <h4 className="card-h4" onClick={ () => setOpen2(!open2)}>Proyecto React + Springboot</h4>
                             <Collapse in={open2}>
                                 <div className={`colapse-text-${theme}`}>
-                                    
+                                    Demostración de desarrollo full stack. Utilizando el framework React en el frontend y haciendo uso de API Rest en backend con springboot y MySQL.
                                 </div>
                             </Collapse>
                             <div className="buttons-project">
-                            <Button className={`button-project-${theme}`} href="#" target={"_blank"} rel="noreferrer">Demo</Button>
-                            <Button className={`button-project-${theme}`} href="#" target={"_blank"} rel="noreferrer">Repositorio</Button>
+                            <Button className={`button-project-${theme}`} href="https://react-springboot-full.netlify.app/" target={"_blank"} rel="noreferrer">Demo</Button>
+                            <Button className={`button-project-${theme}`} href="https://github.com/ArturoCarmona/React-Springboot-Frontend" target={"_blank"} rel="noreferrer">Repositorio</Button>
                             </div>
                             
                         </div>
